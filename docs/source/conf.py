9e108ce70998abbf79001d171edda53d9d101efb
Gaia Saveri <gaia.saveri@gmail.com>
2025-09-29 12:12:48 +0200

    update requirements.txt

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -139,7 +139,8 @@ extlinks = {
 }
 
 autodoc_typehints = "none"
-autodoc_mock_imports = ["torch", "langchain_huggingface", "langchain_community", "langchain_qdrant", "qdrant_client"]
+autodoc_mock_imports = ["torch", "langchain_huggingface", "langchain_community", "langchain_qdrant", "qdrant_client",
+                        "tensorflow", "flax", "faiss", "qdrant"]
 
 templates_path = ['_templates']
 exclude_patterns = ['.DS_Store']
